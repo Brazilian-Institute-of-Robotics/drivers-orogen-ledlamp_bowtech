98b0fae2b6ba38e243a784fd7f850140a885bad1
Diego Cesar <diego.cesar@dfki.de>
2015-04-17 16:23:16 +0200

    First commit

--- ledlamp_bowtechTypes.hpp
+++ ledlamp_bowtechTypes.hpp
@@ -7,8 +7,23 @@
  * However, it is common that you will only import types from your library, in
  * which case you do not need this file
  */
+#include <stdint.h>
+#include <string>
 
 namespace ledlamp_bowtech {
+
+	struct LedAttributes
+	{
+		uint8_t address;
+		uint8_t light_level;
+		uint8_t power_up_light_level;
+	};
+
+	struct LedLamp
+	{
+		std::string name;
+		LedAttributes attribute;
+	};
 }
 
 #endif
